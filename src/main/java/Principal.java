b6f195960a4b11b917ad76f7dd76bad3180e71bd
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-03-28 22:34:05 -0300

    Aula 1 - Criando, atualizando e removendo documento

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Date;
-
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -9,33 +6,32 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
 public class Principal {
-	
+
 	public static void main(String[] args) {
-		MongoClient cliente = new MongoClient();
-		MongoDatabase bancoDeDados = cliente.getDatabase("test");
-		MongoCollection<Document> alunos = bancoDeDados.getCollection("alunos");
-		Document aluno = alunos.find().first();
-		System.out.println(aluno);
-		
-		Document novoAluno = new Document("nome", "Joao")
-			.append("data_nascimento" , new Date(2003, 10, 10))
-			.append("curso" , new Document("nome", "Historia"))
-			.append("notas" , Arrays.asList(10,9,8))
-			.append("habilidades", Arrays.asList(new Document()
-													.append("nome"  , "Ingles ")
-													.append("nível" , "Básico"),
-													new Document()
-													.append("nome" , "Espanhol")
-													.append("nível" , "Básico")));
-		
-		//alunos.insertOne(novoAluno);
-		
-		alunos.updateOne(Filters.eq("nome", "Julia"), new Document("$set", new Document("dataNascimento" , new Date(2017, 04, 11))));
-		
-		//alunos.deleteOne(Filters.eq("nome" , "Joao Silva"));
-		
-		
-		cliente.close();
+		MongoClient cliente = new MongoClient(); // conectar ao mongo
+		MongoDatabase bancoDeDados = cliente.getDatabase("test");// selecionar o banco de dados do qual queremos
+																	// realizar as consultas
+		MongoCollection<Document> alunos = bancoDeDados.getCollection("alunos");// selecionar uma coleção específica
+		Document aluno = alunos.find().first();// selecionaremos o primeiro documento dessa coleção
+		// System.out.println(aluno);
+
+		// Document novoAluno = new Document("nome", "Joao").append("data_nascimento",
+		// new Date(2003, 10, 10)).append("curso", new Document("nome",
+		// "Historia")).append("notas", Arrays.asList(10, 9, 8)).append("habilidades",
+		// Arrays.asList(new Document().append("nome", "Ingles ").append("nível",
+		// "Básico"), new Document().append("nome", "Espanhol").append("nível",
+		// "Básico")));
+
+		// System.out.println(novoAluno);
+
+		// alunos.insertOne(novoAluno);
+
+		// alunos.updateOne(Filters.eq("nome", "Joao"), new Document("$set", new
+		// Document("nome", "Joao Pedro")));
+
+		alunos.deleteOne(Filters.eq("nome", "Joao Silva")); // remove um registro com nome igual a Joao Silva
+
+		cliente.close();// fechar conexão com bd
 	}
 
 }
